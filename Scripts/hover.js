1c2867afff9a4ccad926fc9c15f83c8ff6335f69
MATALAME <141719513+MATALAME@users.noreply.github.com>
2024-04-22 16:27:45 +0200

    minor changes

---
+++ Scripts/hover.js
@@ -0,0 +1,20 @@
+const weeks = [
+    "Week1Hover", 
+    "Week2Hover", 
+    "Week3Hover", 
+    "Week4Hover", 
+    "Week6Hover", 
+    "Week8Hover"
+];
+
+weeks.forEach(weekId => {
+    const weekHover = document.getElementById(weekId);
+
+    weekHover.addEventListener("mouseover", event => {
+        weekHover.innerText = weekHover.innerText.toUpperCase();
+    });
+
+    weekHover.addEventListener("mouseout", event => {
+        weekHover.innerText = weekHover.innerText.toLowerCase();
+    });
+});
--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,31 +1,32 @@
 const menuItems = [
+    { name: "Home", href: "index.html"},
+    { name: "Blogs", href: "blogpage.html" },
+    { name: "Design", href: "design.html" },
+    { name: "Portfolio", href: "portfolio.html" },
+    { name: "Essays", href: "essaypage.html" },
+    { name: "Contact", href: "contact.html" },
+];
 
-    {name: "Blogs", href: "Blogs/week1.html"},
-    {name: "Design", href: "design.html"},
-    {name: "Portfolio", href: "Portfolio/portfolio.html"},
-    {name: "Essays", href: "Essays/essay1.html"},
-    {name: "Contact", href: "contact.html"},
-]
-    export function initialise (currentPage){
+export function initialise(currentPage) {
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
 
-    const nav = document.querySelector("header > nav")
-    const ul = document.createElement ("ul")
-    const li = document.createElement ("li")
-    for (let menuItem of menuItems){
-        const li = document.createElement("li")
-        if (currentPage != menuItem.name){
-            const a = document.createElement("a")    
-            a.innerText = menuItem.name 
-            a.setAttribute("href", menuItem.href)
-            li.appendChild(a)
-            ul.appendChild(li)
-        } else {li.innerText = menuItem.name}
-        ul.appendChild(li)
-    
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        if (currentPage !== menuItem.name) {
+            a.innerText = menuItem.name;
+            a.setAttribute("href", menuItem.href);
+            li.appendChild(a);
+        } else {
+            li.innerText = menuItem.name;
+        }
+
+        ul.appendChild(li);
     }
-     nav.appendChild(ul)
-     
+
+    nav.appendChild(ul);
 }
 
-   
 
