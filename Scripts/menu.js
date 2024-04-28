66777c42ea5e4ec2110988ce0f41581d45567b39
MATALAME <141719513+MATALAME@users.noreply.github.com>
2024-04-28 16:35:30 +0200

    checking root

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -3,10 +3,10 @@ const root = "WSOA3028A_2538590";
 const menuItems = [
     { name: "Home", href: root + "/index.html"},
     { name: "Blogs", href: `${root}/Blogs/index.html` },
-    { name: "Design", href: `${root}/Design/design.html` },
-    { name: "Portfolio", href: `${root}/Portfolio/portfolio.html` },
+    { name: "Design", href: `${root}/Design/index.html` },
+    { name: "Portfolio", href: `${root}/Portfolio/index.html` },
     { name: "Essays", href: `${root}/Essays/index.html` },
-    { name: "Contact", href: `${root}/Contact/contact.html` },
+    { name: "Contact", href: `${root}/Contact/index.html` },
 ];
 
 export function initialise(currentPage) {
