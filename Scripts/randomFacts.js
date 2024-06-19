537edf960fa96aa8e06ef43fde22b8ee4bea3e6e
MATALAME <141719513+MATALAME@users.noreply.github.com>
2024-06-19 21:49:59 +0200

    Minor Changes

--- Scripts/randomFacts.js
+++ Scripts/randomFacts.js
@@ -1,15 +1,15 @@
 const facts = [ // Array of random facts about me
     "I'm a student at University of Witwatersrand.",
     "I'm a car guy.",
-    "My second name is Bernard'.",
+    "My second name is 'Bernard'.",
     "My home language is Sepedi.",
     "I finished matric in 2021.",
     "I have two brothers.",
     "I want a pet, preferably a dog.",
     "I'm a gamer.",
-    "I work for a charity organization in my free time",
+    "I work for a charity organization in my spare time",
     "I love interior design.",
-    "I'm learning how to code in JavaScript."
+    "I've learnt how to code in C#, HTML, JavaScript and CSS."
   ];
   
   function generateFact() { //function that generates random facts
